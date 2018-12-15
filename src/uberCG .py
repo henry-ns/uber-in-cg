5abdef5d1b3ac1f70c053462249192110740b16e
Henrique Martins <enrque.ns@gmail.com>
2018-12-15 08:49:12 -0300

    refactoring

--- src/uberCG .py
+++ src/uberCG .py
@@ -1,16 +1,16 @@
-import time
 import datetime as dt
-import pandas as pd
-
 import json
+import time
+
+import pandas as pd
 
 from uber_rides.session import Session
 from uber_rides.client import UberRidesClient
 
-UberToken = 'token'
-ProductId = 'd19e0080-4039-4a6f-97de-d00e0f43f3cc'
+uber_token = 'token'
+product_id = 'd19e0080-4039-4a6f-97de-d00e0f43f3cc'
 
-session = Session(server_token=UberToken)
+session = Session(server_token=uber_token)
 client = UberRidesClient(session)
 
 data = open('../res/geo.json')
@@ -19,11 +19,14 @@ points = json.load(data)
 time = dt.datetime.now()
 nbhoods_data = {}
 for point in points:
-    response = client.get_pickup_time_estimates(     
+    response = client.get_pickup_time_estimates(
         start_latitude=point['lat'],
         start_longitude=point['lng'],
-        product_id=ProductId)
+        product_id=product_id
+    )
+
     estimate = response.json.get('times')[0]['estimate']
-    nbhoods_data[point['address']] = pd.Series([estimate], index=[time_before])
+    nbhoods_data[point['address']] = pd.Series([estimate], index=[time])
+
 dataFrame = pd.DataFrame(nbhoods_data)
 dataFrame.to_csv('bairros_estimativas.csv', mode='a', header=False)

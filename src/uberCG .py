81d2d592c861065fecda679cd4d6bb633edf9119
Felipe <34188166+felipefbs@users.noreply.github.com>
2018-12-15 18:09:25 -0300

    coleta dados a cada 30 min

--- src/uberCG .py
+++ src/uberCG .py
@@ -1,7 +1,6 @@
 import datetime as dt
 import json
 import time
-
 import pandas as pd
 
 from uber_rides.session import Session
@@ -13,20 +12,23 @@ product_id = 'd19e0080-4039-4a6f-97de-d00e0f43f3cc'
 session = Session(server_token=uber_token)
 client = UberRidesClient(session)
 
-data = open('../res/geo.json')
+data = open('..\\res\\geo.json')
 points = json.load(data)
 
-time = dt.datetime.now()
-nbhoods_data = {}
-for point in points:
-    response = client.get_pickup_time_estimates(
-        start_latitude=point['lat'],
-        start_longitude=point['lng'],
-        product_id=product_id
-    )
+while True:
+    horario = dt.datetime.now()
+    nbhoods_data = {}
+    for point in points:
+        response = client.get_pickup_time_estimates(
+            start_latitude=point['lat'],
+            start_longitude=point['lng'],
+            product_id=product_id
+        )
+        estimate = response.json.get('times')[0]['estimate']
+        nbhoods_data[point['address']] = pd.Series([estimate], index=[horario])
+        print(estimate)
 
-    estimate = response.json.get('times')[0]['estimate']
-    nbhoods_data[point['address']] = pd.Series([estimate], index=[time])
-
-dataFrame = pd.DataFrame(nbhoods_data)
-dataFrame.to_csv('bairros_estimativas.csv', mode='a', header=False)
+    dataFrame = pd.DataFrame(nbhoods_data)
+    dataFrame.to_csv('bairros_estimativas.csv', mode='a', header=False)
+    print('waiting 30 minutes')
+    time.sleep(1800)
